943c067ad296d8a1f6cbff4c134846c007bbb891
icaro gavazza <icarodgl@gmail.com>
2022-02-18 19:10:50 -0300

    datas

--- public/main.js
+++ public/main.js
@@ -1,105 +1,129 @@
-async function getDados(){
-    let url =  'https://temperatura-api.herokuapp.com/dados' //
-
-    var myInit = { method: 'GET',
-             headers: {"Content-Type":"application/json"},
-             mode: 'cors',
-             cache: 'default' };
-
-  var myRequest = new Request(url, myInit);
-
-   
-  let dados = await fetch(myRequest);
-
-  dados = await dados.json()
-  return dados
-}
-async function createChartsTemperatura(dados){
-    
-  let labels = dados.map(x => {return new Date(x.data).toLocaleString() } )
-  let temperatura = dados.map(x => {return  x.temperatura1 })
-  let temperatura2 = dados.map(x => {return x.temperatura2})
-  
-  const ctx = document.getElementById('temperatura').getContext('2d');
-
-  
-  const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: labels,
-          datasets: [{
-              label: '# Temperatura 1',
-              data: temperatura,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.5)',
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-              ],
-              borderWidth: 1
-          },
-          {
-              label: '# Temperatura 2',
-              data: temperatura2,
-              
-              backgroundColor: [
-                  'rgba(255, 138, 4, 0.5)',
-              ],
-              borderColor: [
-                  'rgba(255, 138, 4, 1)',
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          plugins:{align:"center",color:'red',display:true,text:"aaaaaaa "},
-          scales:{parsing: false},
-          layout:{autoPadding:true},
-          interaction: {
-          mode: 'index'
-      },
-      scales: {
-      }
-      }
-  });
-  }
-
-  async function createChartsHumidade(dados){
-
-  let labels = dados.map(x => {return new Date(x.data).toLocaleString() } )
-  let humidade= dados.map(x => {return  x.humidade })
-  const ctx = document.getElementById('humidade').getContext('2d');
-  const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: labels,
-          datasets: [{
-              label: '# humidade',
-              data: humidade,
-              backgroundColor: [
-                  'rgba(58, 4, 255, 0.5)',
-              ],
-              borderColor: [
-                  'rgba(58, 4, 255, 1)',
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-        scales:{parsing: false},
-        layout:{autoPadding:true},
+const TIPO = { "temperatura": "temperatura1", "humidade": "humidade" }
+var DADOS = []
+var ctx = document.getElementById('humidade').getContext('2d');
+var chartHumidade = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# humidade',
+            data: [],
+            backgroundColor: [
+                'rgba(58, 4, 255, 0.5)',
+            ],
+            borderColor: [
+                'rgba(58, 4, 255, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: { parsing: false },
+        layout: { autoPadding: true },
         interaction: {
-        mode: 'index'
-      },
-      scales: {
-      }
-      }
-  });
-  }
-  async function main(){
-      let dados = await getDados()
-    createChartsTemperatura(dados)
-    createChartsHumidade(dados)
-  }
+            mode: 'index'
+        },
+        scales: {
+        }
+    }
+});
+var ctx = document.getElementById('temperatura').getContext('2d');
+var chartTemperatura = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# Temperatura',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        },
+        ]
+    },
+    options: {
+        plugins: { align: "center", color: 'red', display: true, text: "aaaaaaa " },
+        scales: { parsing: false },
+        layout: { autoPadding: true },
+        interaction: {
+            mode: 'index'
+        },
+        scales: {
+        }
+    }
+})
+
+
+async function getDados() {
+    let url = 'https://temperatura-api.herokuapp.com/dados' //
+
+    var myInit = {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" },
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    var myRequest = new Request(url, myInit);
+
+
+    let dados = await fetch(myRequest);
+
+    dados = await dados.json()
+    return dados
+}
+async function createChart(chart,dados) {
+    chart.clear()
+    chart.data.labels = dados.labels
+    chart.data.datasets.forEach(dataset => {
+        console.log("DATASET", dataset);
+        dataset.data = dados.dados
+    });
+    chart.update()
+}
+
+
+function filtraDados(dados, tipo, inicio, fim) {
+    let resp = {
+        "dados": [],
+        "labels": []
+    }
+    if (inicio !== "" && fim !== "") {
+        inicio = new Date(inicio)
+        fim = new Date(fim)
+        
+        dados.forEach(x => {
+            let data = new Date(x.data)
+            if (inicio.getTime() < data.getTime() <= fim.getTime()) {
+                console.log("true");
+                resp.labels.push(data.toLocaleString())
+                resp.dados.push(x[tipo])
+            }
+        })
+    } else {
+        dados.forEach(x => {
+            let data = new Date(x.data).toLocaleString()
+            resp.labels.push(data)
+            resp.dados.push(x[tipo])
+        })
+    }
+    console.log("RESP>>",resp);
+    return resp
+}
+
+function updateFiltro(inicio = new Date(), fim = new Date()) {
+    let temp = filtraDados(DADOS, TIPO.temperatura, inicio, fim)
+    let humi = filtraDados(DADOS, TIPO.humidade, inicio, fim)
+    createChart(chartTemperatura,temp)
+    createChart(chartHumidade,humi)
+}
+async function main() {
+    DADOS = await getDados()
+    updateFiltro()
+}
 
 main()
